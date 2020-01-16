bfd6fcc7e5dd44e3ccbfecb718cbf396ceb9a6ce
Junming Zheng <beneon@qq.com>
2020-01-16 11:32:33 +0800

    articles start

--- django_beginner/settings.py
+++ django_beginner/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     # local
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
+    'articles.apps.ArticlesConfig',
 ]
 
 MIDDLEWARE = [
--- django_beginner/urls.py
+++ django_beginner/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('users/',include('users.urls')),
     path('users/',include('django.contrib.auth.urls')),
     path('accounts/',include('django.contrib.auth.urls')),
+    path('articles/',include('articles.urls')),
     path('',include('pages.urls')),
 ]
